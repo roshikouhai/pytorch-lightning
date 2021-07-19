2bceef0e596b25817c35b34e05b96fecf964e7ad
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-20 00:23:18 +0200

    x

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -118,7 +118,7 @@ def run():
             f"save_dir: {trainer.logger.log_dir} "
         )
 
-        with open(os.path.join(trainer.logger.log_dir, str(trainer.global_rank) + ".txt"), "w") as file:
+        with open(os.path.join(trainer.logger.log_dir, str(trainer.global_rank) + "-" + str(i) + ".txt"), "w") as file:
             file.write(str(trainer.global_rank))
 
 
